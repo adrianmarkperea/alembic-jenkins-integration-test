a42bf23a082ed6d5d586f5769230b664ed6f7f34
Adrian Perea <adrianmarkperea@gmail.com>
2022-07-16 16:06:58 +0800

    Add hostname ip address

--- execute.sh
+++ execute.sh
@@ -1,7 +1,7 @@
 docker run \
   -e DB_USER="postgres" \
   -e DB_PASSWORD="postgres" \
-  -e DB_HOST="host.network.internal" \
+  -e DB_HOST="172.17.0.1" \
   -e DB_PORT="5432" \
   -e DB_NAME="postgres" \
   alembic-runner

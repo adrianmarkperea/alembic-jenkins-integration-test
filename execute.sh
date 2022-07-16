c9eff110fd92520f17527060f91afc9b2a042eb2
Adrian Perea <adrianmarkperea@gmail.com>
2022-07-16 16:18:41 +0800

    Add docker-compose.yaml

--- execute.sh
+++ execute.sh
@@ -1,7 +1,7 @@
 docker run \
   -e DB_USER="postgres" \
   -e DB_PASSWORD="postgres" \
-  -e DB_HOST="172.17.0.1" \
+  -e DB_HOST="postgres" \
   -e DB_PORT="5432" \
   -e DB_NAME="postgres" \
   alembic-runner

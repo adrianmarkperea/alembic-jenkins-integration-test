b034c553fadf6b018ac78c93c34dcd8c1292c5b1
Adrian Perea <adrianmarkperea@gmail.com>
2022-07-16 16:41:34 +0800

    Update DB hostname

--- execute.sh
+++ execute.sh
@@ -1,7 +1,7 @@
 docker run \
   -e DB_USER="postgres" \
   -e DB_PASSWORD="postgres" \
-  -e DB_HOST="postgres" \
+  -e DB_HOST="alembic-test-postgres-1" \
   -e DB_PORT="5432" \
   -e DB_NAME="postgres" \
   alembic-runner

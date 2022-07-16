96c5a7bc43a924607116ac57075755596f4f9e77
Adrian Perea <adrianmarkperea@gmail.com>
2022-07-16 15:39:24 +0800

    Remove network=host from execute.sh

--- execute.sh
+++ execute.sh
@@ -1,9 +1,8 @@
 docker run \
   -it \
-  --network="host" \
   -e DB_USER="postgres" \
   -e DB_PASSWORD="postgres" \
-  -e DB_HOST="localhost" \
+  -e DB_HOST="host.network.internal" \
   -e DB_PORT="5432" \
   -e DB_NAME="postgres" \
   alembic-runner
